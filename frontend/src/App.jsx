9b4eff152ef8666c66a88f3e673dc2b3ea08ed4a
shaktisingh647 <consolelogshakti@gmail.com>
2026-01-03 20:20:04 +0530

    first-commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     Hiiii
+     <h1 class="text-3xl font-bold underline">
+    Hiiii
+  </h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(process.env.PORT ||5000, (res,req)=>{
+    console.log(`server is listening on port ${server}`)
+})
